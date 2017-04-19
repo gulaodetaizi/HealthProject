58833dce9f354c79af3509e618567982ee3fef74
Taixiu Zhang <taixiu.zhang@gmail.com>
2017-04-19 14:29:23 +0800

    create health project.

---
+++ Bodylogical/Bodylogical/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bodylogical
+//
+//  Created by appledev096 on 17/4/19.
+//  Copyright © 2017年 Person. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
